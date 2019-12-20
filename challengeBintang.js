142bcd197202c275868d8ca850ba8da3f67ae43d
ayukhairina <wahyukhairina@gmail.com>
2019-12-20 19:24:22 +0800

    segitiga bintang

---
+++ challengeBintang.js
@@ -0,0 +1,14 @@
+for (let baris = 7; baris >0; baris--) {
+    let hasil = '';
+    for (let kolom = 1; kolom < baris; kolom ++) {
+        hasil += '*';
+    }
+    console.log(hasil);
+}
+//hasil :
+//******
+//*****
+//****
+//***
+//**
+//*
